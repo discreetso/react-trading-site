566bc69b92eb51150a0b503293439821dda4b540
discreetso <visualcrn.001@gmail.com>
2025-11-17 20:43:34 +0500

    implement 'about' component

--- src/landing_page/about/Team.jsx
+++ src/landing_page/about/Team.jsx
@@ -1,6 +1,41 @@
 function Team() {
     return (
-        <h1>Team</h1>
+        <div className="container">
+            <div className="row p-3 mt-5 border-top">
+                <h1 className="text-center">People</h1>
+            </div>
+
+            <div className="row p-5 text-muted"
+            style={{ lineHeight: '1.3', fontSize: '1.2em'}}>
+                <div className="col-6 p-5 text-center">
+                    <img src="media/images/nithinKamath.jpg" alt="OwnerImage" style={{ borderRadius:'100%', width:'60%' }} />
+                    <h4 className="mt-5">Nithin Kamath</h4>
+                    <h6>Founder, CEO</h6>
+                </div>
+                    
+                <div className="col-6 p-3">
+                    <p>
+                        Nithin Kamath is an Indian entrepreneur, best known as the
+                        co-founder and CEO of Zerodha, India's largest stock brokerage firm. 
+                        He is recognized for revolutionizing the Indian brokerage industry by 
+                        pioneering a low-cost, flat-fee model for trading. 
+                    </p>
+                    <p>
+                        Kamath began his career in the stock market at the age of 17 and was an
+                        active trader for over a decade before founding Zerodha. He worked 
+                        night shifts at a call center to fund his daytime trading in his early years.
+                    </p>
+                    <p>
+                        He was named EY Entrepreneur Of The Year 2024 for his contributions to 
+                        financial services industry
+                    </p>
+                    <p>
+                        Connect on <a href="#">HomePage</a> / <a href="#">Twitter</a> /  
+                        <a href="#"> Trading QnA</a>
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
