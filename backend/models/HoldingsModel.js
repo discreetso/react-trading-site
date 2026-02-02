b9234068f29a2e7816fd1f939e21cd001ef1a6a7
discreetso <visualcrn.001@gmail.com>
2026-02-02 11:41:02 +0500

    Update HoldingsModel.js with comments
    Added comments for database credentials and ensured proper module export format.

--- backend/models/HoldingsModel.js
+++ backend/models/HoldingsModel.js
@@ -5,3 +5,7 @@ const {HoldingsSchema} = require('../schemas/HoldingsSchema');
 const HoldingsModel = new model("holding", HoldingsSchema);
 
 module.exports = { HoldingsModel };
+
+// TradFi Bulls database cluster username and password
+// username: gtasphalt9_db_user
+// password: deVt3rGAwYg8675N  
